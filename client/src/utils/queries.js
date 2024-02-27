17daacec2cf443e611bde5c93e48e52ddefb8523
Luis Rubio <luisrubio7771@outlook.com>
2024-02-26 18:06:26 -0600

    added my queries and mutations in utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// Setup for Apollo Client and GraphQl
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
